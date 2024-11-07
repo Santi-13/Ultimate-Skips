40a4e9b1f65158ca1b2c5021bb349137c3aaed12
Santiago P <s.penunuri@hotmail.com>
2024-11-07 17:55:03 -0600

    Last restructuration I promise

--- build/vision-skips/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/sanmaster/ultimate_skips/Ultimate-Skips/install/vision-skips'
---
+++ src/explorer_skips/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'explorer_skips'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/exploration.launch.py']),
+        ('share/' + package_name + '/config', ['config/navigation.yaml', 'config/slam.yaml']),
+        ('share/' + package_name + '/rviz', ['rviz/explore.rviz']),  
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Sanmaster5',
+    maintainer_email='s.penunuri@hotmail.com',
+    description="Package that manages skip's mapping, navigation and exploration algorithms.",
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'left_wall_follower = explorer_skips.left_wall_follower:main',
+            'maze_escape_robot = explorer_skips.maze_escape_robot:main',
+            'pledge_algorithm = explorer_skips.pledge_algorithm:main'
+            'nav_goal_sender = explorer_skips.nav_goal_sender:main'
+        ],
+    },
+)

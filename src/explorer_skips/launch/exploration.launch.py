41501401740c0055fc5452989b832127b1be5a55
Santiago P <s.penunuri@hotmail.com>
2024-11-12 17:28:37 -0600

    abandoned gazebo dreams

--- src/explorer_skips/launch/exploration.launch.py
+++ src/explorer_skips/launch/exploration.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
@@ -9,25 +9,23 @@ def generate_launch_description():
     from ament_index_python.packages import get_package_share_directory
 
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
-    slam_toolbox_dir = get_package_share_directory('slam_toolbox')
     explorer_skips_dir = get_package_share_directory('explorer_skips')
 
     # Define launch arguments
-    world_arg = DeclareLaunchArgument('world', default_value=os.path.join(explorer_skips_dir, 'worlds', 'map_world.world'))
-
     nav2_params = os.path.join(explorer_skips_dir, 'config', 'navigation.yaml')
     slam_params = os.path.join(explorer_skips_dir, 'config', 'slam.yaml')
 
+    declared_arguments = []
 
 
-    return LaunchDescription([
+    return LaunchDescription(declared_arguments + [
         Node(
             package='tf2_ros',
             namespace = 'scan_to_map',
             executable='static_transform_publisher',
             arguments= ["0", "0", "0", "0", "0", "0", "map", "scan"]
         ),
-        # Node de rviz2
+        # Nodo de rviz2
         Node(
             package='rviz2',
             executable='rviz2',
@@ -39,7 +37,6 @@ def generate_launch_description():
             package='slam_toolbox',
             executable='async_slam_toolbox_node',
             name='slam_toolbox',
-            output='screen',
             parameters=[slam_params]
         ),
         # Lanzar Navigation2
@@ -54,12 +51,5 @@ def generate_launch_description():
         #    name='nav_goal_sender',
         #    output='screen'
         # ),
-        # Gazebo server node
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')),
-            launch_arguments={'world': LaunchConfiguration('world')}.items(),
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py'))
-        )
+
     ])
